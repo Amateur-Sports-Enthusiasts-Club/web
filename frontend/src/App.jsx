d718ee48483347bf4338d73187e47b159708e5e3
Tejasvi Chebrolu <tejasvicsr1@gmail.com>
2020-09-05 22:02:02 +0530

    Basic events layout done.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,6 +8,11 @@ import Navbar from "./components/Navbar";
 
 // Pages {{{
 import Home from "./pages/Home";
+import Football from "./pages/Football";
+import Cricket from "./pages/Cricket";
+import Basketball from "./pages/Basketball";
+import Badminton from "./pages/Badminton";
+import Pool from "./pages/Pool";
 // }}}
 
 export function App() {
@@ -17,11 +22,11 @@ export function App() {
             <Switch>
                 <Route exact path="/" component={Home} />
 
-                {/* <Route exact path="/football" component={Football} /> */}
-                {/* <Route exact path="/cricket" component={Cricket} /> */}
-                {/* <Route exact path="/Basketball" component={Basketball} /> */}
-                {/* <Route exact path="/Badminton" component={Badminton} /> */}
-                {/* <Route exact path="/pool" component={Pool} /> */}
+                <Route exact path="/football" component={Football} />
+                <Route exact path="/cricket" component={Cricket} />
+                <Route exact path="/Basketball" component={Basketball} />
+                <Route exact path="/Badminton" component={Badminton} />
+                <Route exact path="/pool" component={Pool} />
 
                 {/* <Route exact path="/404" component={Error404} /> */}
                 <Redirect to="/404" />
