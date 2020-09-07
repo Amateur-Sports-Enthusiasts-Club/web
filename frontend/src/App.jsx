ca6176dc10408b6306ecec373087fa6bedf5244d
AurumnPegasus <subramanian.shivansh@gmail.com>
2020-09-07 15:46:15 +0530

    Transition between pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Switch, Route, Redirect } from "react-router-dom";
+import { AnimatedSwitch, spring, AnimatedRoute } from 'react-router-transition';
 import "./App.css";
 
 // Components {{{
@@ -15,11 +16,48 @@ import Badminton from "./pages/Badminton";
 import Pool from "./pages/Pool";
 // }}}
 
+function mapStyles(styles) {
+    return {
+      opacity: styles.opacity,
+      transform: `scale(${styles.scale})`,
+    };
+  }  
+
+function bounce(val) {
+    return spring(val, {
+      stiffness: 330,
+      damping: 22,
+    });
+}
+
 export function App() {
+    const bounceTransition = {
+        // start in a transparent, upscaled state
+        atEnter: {
+          opacity: 0,
+          scale: 1.2,
+        },
+        // leave in a transparent, downscaled state
+        atLeave: {
+          opacity: bounce(0),
+          scale: bounce(0.8),
+        },
+        // and rest at an opaque, normally-scaled state
+        atActive: {
+          opacity: bounce(1),
+          scale: bounce(1),
+        },
+    };
     return (
         <>
             <Navbar />
             <Switch>
+            <AnimatedSwitch
+                atEnter={bounceTransition.atEnter}
+                atLeave={bounceTransition.atLeave}
+                atActive={bounceTransition.atActive}
+                mapStyles={mapStyles}
+                className="switch-wrapper">
                 <Route exact path="/" component={Home} />
 
                 <Route exact path="/football" component={Football} />
@@ -30,6 +68,7 @@ export function App() {
 
                 {/* <Route exact path="/404" component={Error404} /> */}
                 <Redirect to="/404" />
+            </AnimatedSwitch>
             </Switch>
         </>
     );
